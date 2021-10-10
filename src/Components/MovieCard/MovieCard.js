081c1c6a08d2b7e07b4ba93993324404608fbf42
Mehmet-Erdem-Akin <mehmet.erdem.akin@outlook.com>
2021-10-10 17:09:14 +0300

    added lazy loadf component

--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -3,32 +3,44 @@ import "./MovieCard.css";
 import MovieRating from "../MovieRating/MovieRating";
 import ThumbsBar from "../ThumbsBar/ThumbsBar";
 import MaxLineText from "../MaxLineText/MaxLineText";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 export default function MovieCard(props) {
   return (
     <div>
       <div className="card-header">
-        <img
+        <LazyLoadImage
           className="image"
-          src={props.img}
-          alt=""
-          srcset=""
           style={{
             borderRadius: 8,
             boxShadow: "0 2px 8px rgb(0 0 0 / 10%)",
             minHeight: "calc(150px * 1.5)",
-            height: "calc(150px * 1.5)",
+            height: "200px",
             background: "#dbdbdb",
             overflow: "hidden",
           }}
-        ></img>
+          effect="blur"
+          src={props.img}
+        />
+
         <div className="card-feature">
           <div className="movie-rating">
             <MovieRating
-             rating={props.rating}
-             trailColor={(props.rating >= 70) === true && '#204529' || (props.rating < 70 && props.rating >= 40) === true && '#3B3910' || (props.rating < 40) === true && '#571435' }
-             pathColor={(props.rating >= 70) === true && '#21d07a' || (props.rating < 70 && props.rating >= 40) === true && '#D2D531' || (props.rating < 40) === true && '#DB2360' }
-             />
+              rating={props.rating}
+              trailColor={
+                (props.rating >= 70 === true && "#204529") ||
+                ((props.rating < 70 && props.rating >= 40) === true &&
+                  "#3B3910") ||
+                (props.rating < 40 === true && "#571435")
+              }
+              pathColor={
+                (props.rating >= 70 === true && "#21d07a") ||
+                ((props.rating < 70 && props.rating >= 40) === true &&
+                  "#D2D531") ||
+                (props.rating < 40 === true && "#DB2360")
+              }
+            />
           </div>
           <div className="thumbs">
             <ThumbsBar />
@@ -36,14 +48,12 @@ export default function MovieCard(props) {
         </div>
       </div>
       <div className="card-body">
-          <MaxLineText 
-            maxLine="3"
-          >
+        <MaxLineText maxLine="3">
           <h2>
-          <a href="">{props.title}</a>
-        </h2>
-          </MaxLineText>
-        
+            <a href="">{props.title}</a>
+          </h2>
+        </MaxLineText>
+
         <p>{props.date}</p>
       </div>
     </div>
